37848062444e3e27b03a4f06df6c31a90b251682
James Stewart <jys@ketlai.co.uk>
2011-09-12 14:12:30 +0100

    Set up "breadcrumbs" appropriately
    If a page has the meta tags x-section-name and x-section-link we can build the appropriate content for the "breadcrumb" trail

--- lib/slimmer.rb
+++ lib/slimmer.rb
@@ -131,6 +131,25 @@ module Slimmer
     end
   end
 
+  class SectionInserter
+    def filter(src,dest)
+      meta_name = dest.at_css('meta[name="x-section-name"]')
+      meta_link = dest.at_css('meta[name="x-section-link"]')
+      list = dest.at_css('nav[role=navigation] ol')
+      
+      if meta_name && meta_link && list
+        link_node = Nokogiri::XML::Node.new('a', dest)
+        link_node['href'] = meta_link['content']
+        link_node.content = meta_name['content']
+        
+        list_item = Nokogiri::XML::Node.new('li', dest)
+        list_item.add_child(link_node)
+
+        list.first_element_child.after(list_item)
+      end
+    end
+  end
+
   class TagMover
     def filter(src,dest)
       move_tags(src, dest, 'script', :must_have => ['src'])
@@ -245,7 +264,8 @@ module Slimmer
       processors = [
         TitleInserter.new(),
         TagMover.new(),
-        BodyInserter.new()
+        BodyInserter.new(),
+        SectionInserter.new()
       ]
 
       self.process(processors,body,template('wrapper'))
