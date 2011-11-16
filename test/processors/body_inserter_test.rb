80c023f160c4704306cac0e07206090c402f7ab1
James Adam <james@lazyatom.com>
2011-11-16 14:42:20 +0000

    Improve this test.
    Thinking about it a bit more, I now understand that the processors modify the template, so that's what we should be testing.

--- test/processors/body_inserter_test.rb
+++ test/processors/body_inserter_test.rb
@@ -5,26 +5,26 @@ class BodyInserterTest < MiniTest::Unit::TestCase
     template = as_nokogiri %{
       <html><body><div><div id="wrapper"></div></div></body></html>
     }
-    expected = %{
-      <div id="wrapper"><p>this should be moved</p></div>
-    }.strip
     source = as_nokogiri %{
       <html><body><nav></nav><div id="wrapper"><p>this should be moved</p></div></body></html>
     }
-    assert_equal expected, Slimmer::BodyInserter.new.filter(source, template).to_s
+
+    Slimmer::BodyInserter.new.filter(source, template)
+    assert_in template, "#wrapper", %{<p>this should be moved</p>}
   end
 
   def test_should_allow_replacement_of_arbitrary_wrappers
     template = as_nokogiri %{
-      <html><body><div><div id="some_other_id"></div></div></body></html>
+      <html><body><div>
+      <div id="wrapper">don't touch this</div>
+      <div id="some_other_id"></div></div></body></html>
     }
-    expected = %{
-      <div id="some_other_id"><p>this should be moved</p></div>
-    }.strip
     source = as_nokogiri %{
-      <html><body><div id="wrapper">don't touch this</div>
-        <div id="some_other_id"><p>this should be moved</p></div></body></html>
+      <html><body><div id="some_other_id"><p>this should be moved</p></div></body></html>
     }
-    assert_equal expected, Slimmer::BodyInserter.new("#some_other_id").filter(source, template).to_s
+
+    Slimmer::BodyInserter.new("#some_other_id").filter(source, template)
+    assert_in template, "#wrapper", %{don't touch this}
+    assert_in template, "#some_other_id", %{<p>this should be moved</p>}
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,4 +5,8 @@ class MiniTest::Unit::TestCase
   def as_nokogiri(html_string)
     Nokogiri::HTML.parse(html_string.strip)
   end
+
+  def assert_in(template, selector, content, message=nil)
+    assert_equal content, template.at_css(selector).inner_html.to_s, message
+  end
 end
