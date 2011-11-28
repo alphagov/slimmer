cc85357f94c02ef68cefaf3e18a0fd4f2fc760e0
James Adam <james@lazyatom.com>
2011-11-28 17:08:04 +0000

    Autoload is more idiomatic.
    This also means that files aren't loaded unless the corresponding constants are used.

--- lib/slimmer.rb
+++ lib/slimmer.rb
@@ -1,20 +1,3 @@
-require 'slimmer/version'
-
-require 'slimmer/template'
-require 'slimmer/app'
-require 'slimmer/title_inserter'
-require 'slimmer/admin_title_inserter'
-require 'slimmer/section_inserter'
-require 'slimmer/tag_mover'
-require 'slimmer/footer_remover'
-require 'slimmer/skin'
-require 'slimmer/body_inserter'
-require 'slimmer/body_class_copier'
-require 'slimmer/url_rewriter'
-require 'slimmer/header_context_inserter'
-
-require 'slimmer/railtie' if defined?(Rails)
-
 require 'nokogiri'
 require 'erb'
 require 'open-uri'
@@ -23,4 +6,20 @@ require 'plek'
 module Slimmer
   TEMPLATE_HEADER = 'X-Slimmer-Template'
   SKIP_HEADER = 'X-Slimmer-Skip'
+
+  autoload :Version, 'slimmer/version'
+  autoload :Railtie, 'slimmer/railtie'
+  autoload :Skin, 'slimmer/skin'
+
+  autoload :Template, 'slimmer/template'
+  autoload :App, 'slimmer/app'
+  autoload :TitleInserter, 'slimmer/title_inserter'
+  autoload :AdminTitleInserter, 'slimmer/admin_title_inserter'
+  autoload :SectionInserter, 'slimmer/section_inserter'
+  autoload :TagMover, 'slimmer/tag_mover'
+  autoload :FooterRemover, 'slimmer/footer_remover'
+  autoload :BodyInserter, 'slimmer/body_inserter'
+  autoload :BodyClassCopier, 'slimmer/body_class_copier'
+  autoload :UrlRewriter, 'slimmer/url_rewriter'
+  autoload :HeaderContextInserter, 'slimmer/header_context_inserter'
 end
