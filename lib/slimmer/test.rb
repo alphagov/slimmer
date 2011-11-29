db1d94173fed59daa95631a8c64ece77aecb578d
Craig R Webster <craig@barkingiguana.com>
2011-11-29 12:02:46 +0000

    Provide an override for fetching templates in a test environment

---
+++ lib/slimmer/test.rb
@@ -0,0 +1,22 @@
+module Slimmer
+  class Skin
+    def load_template name
+      %q{
+        <html>
+          <head>
+            <title>Test Template</title>
+            <script src="http://static.dev.alphagov.co.uk/javascripts/libs/jquery/jquery-1.6.2.min.js"></script><!-- no defer on jquery -->
+            <script src="http://static.dev.alphagov.co.uk/javascripts/libs/jquery/jquery-ui-1.8.16.custom.min.js" defer></script>
+            <script src="http://static.dev.alphagov.co.uk/javascripts/libs/jquery/plugins/jquery.base64.js" defer></script>
+            <script src="http://static.dev.alphagov.co.uk/javascripts/search.js" defer></script>
+            <script src="http://static.dev.alphagov.co.uk/javascripts/devolution.js" defer></script>
+            <script src="http://static.dev.alphagov.co.uk/javascripts/popup.js" defer></script>
+            <script src="http://static.dev.alphagov.co.uk/javascripts/feedback.js" defer></script>
+            <script src="http://static.dev.alphagov.co.uk/javascripts/customisation-settings.js" defer></script>
+          </head>
+          <body></body>
+        </html>
+      }
+    end
+  end
+end
