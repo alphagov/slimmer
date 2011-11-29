2d8d4a888866fdf4bd0ec3ce792968753bbe3c68
Craig R Webster <craig@barkingiguana.com>
2011-11-29 15:00:06 +0000

    Use preview which can resolve from all environments

--- lib/slimmer/test.rb
+++ lib/slimmer/test.rb
@@ -7,14 +7,14 @@ module Slimmer
         <html>
           <head>
             <title>Test Template</title>
-            <script src="http://static.dev.gov.uk/javascripts/libs/jquery/jquery-1.6.2.min.js"></script><!-- no defer on jquery -->
-            <script src="http://static.dev.gov.uk/javascripts/libs/jquery/jquery-ui-1.8.16.custom.min.js" defer></script>
-            <script src="http://static.dev.gov.uk/javascripts/libs/jquery/plugins/jquery.base64.js" defer></script>
-            <script src="http://static.dev.gov.uk/javascripts/search.js" defer></script>
-            <script src="http://static.dev.gov.uk/javascripts/devolution.js" defer></script>
-            <script src="http://static.dev.gov.uk/javascripts/popup.js" defer></script>
-            <script src="http://static.dev.gov.uk/javascripts/feedback.js" defer></script>
-            <script src="http://static.dev.gov.uk/javascripts/customisation-settings.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts/libs/jquery/jquery-1.6.2.min.js"></script><!-- no defer on jquery -->
+            <script src="http://static.preview.alphagov.co.uk/javascripts/libs/jquery/jquery-ui-1.8.16.custom.min.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts/libs/jquery/plugins/jquery.base64.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts/search.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts.preview.alphagov.lution.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts/popup.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts/feedback.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts/customisation-settings.js" defer></script>
           </head>
           <body>
             <div class="header-context">Header</div>
