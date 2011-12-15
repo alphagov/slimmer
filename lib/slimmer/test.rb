b66c6116a2979dff90518e070db5931f8bdff42a
Craig R Webster <craig@barkingiguana.com>
2011-12-15 12:33:00 +0000

    jQuery mustache is provided.

--- lib/slimmer/test.rb
+++ lib/slimmer/test.rb
@@ -10,6 +10,7 @@ module Slimmer
             <script src="http://static.preview.alphagov.co.uk/javascripts/libs/jquery/jquery-1.6.2.min.js"></script><!-- no defer on jquery -->
             <script src="http://static.preview.alphagov.co.uk/javascripts/libs/jquery/jquery-ui-1.8.16.custom.min.js" defer></script>
             <script src="http://static.preview.alphagov.co.uk/javascripts/libs/jquery/plugins/jquery.base64.js" defer></script>
+            <script src="http://static.preview.alphagov.co.uk/javascripts/libs/jquery/plugins/jquery.mustache.js" defer></script>
             <script src="http://static.preview.alphagov.co.uk/javascripts/search.js" defer></script>
             <script src="http://static.preview.alphagov.co.uk/javascripts/devolution.js" defer></script>
             <script src="http://static.preview.alphagov.co.uk/javascripts/popup.js" defer></script>
