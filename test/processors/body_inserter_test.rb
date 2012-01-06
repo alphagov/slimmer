d1a71e165d39131510e28612b3c9d48e36586efa
James Mead <james@floehopper.org>
2012-01-06 18:01:05 +0000

    Ensure slimmer doesn't mess with unicode characters.
    I expect this test to fail on the continuous integration machine,
    because of a bug in the version of libxml that Nokogiri is using on our
    Linux boxes.
    
    I want to be sure this test fails, before I commit a fix which works
    around the problem. A better solution would be to upgrade
    Nokogiri/libxml to include the relevant bug fix.

--- test/processors/body_inserter_test.rb
+++ test/processors/body_inserter_test.rb
@@ -13,6 +13,19 @@ class BodyInserterTest < MiniTest::Unit::TestCase
     assert_in template, "#wrapper", %{<p>this should be moved</p>}
   end
 
+  def test_should_copy_across_unicode_characters_without_messing_with_their_encoding
+    unicode_endash = [0x2013].pack('U*')
+    template = as_nokogiri %{
+      <html><body><div><div id="wrapper"></div></div></body></html>
+    }
+    source = as_nokogiri %{
+      <html><body><nav></nav><div id="wrapper"><p>#{unicode_endash}</p></div></body></html>
+    }
+
+    Slimmer::BodyInserter.new.filter(source, template)
+    assert_equal unicode_endash, template.at_css("#wrapper p").inner_text
+  end
+
   def test_should_allow_replacement_of_arbitrary_wrappers
     template = as_nokogiri %{
       <html><body><div>
