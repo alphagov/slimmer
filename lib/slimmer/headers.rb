29b5fa6978c4eda01fe31b7a52ada7851bdd772c
David Heath <david@davidheath.org>
2012-01-12 16:36:13 +0000

    Bugfix: headers values should always be strings.

--- lib/slimmer/headers.rb
+++ lib/slimmer/headers.rb
@@ -15,7 +15,7 @@ module Slimmer
       raise InvalidHeader if (hash.keys - SLIMMER_HEADER_MAPPING.keys).any?
       SLIMMER_HEADER_MAPPING.each do |hash_key, header_suffix|
         value = hash[hash_key]
-        headers["X-Slimmer-#{header_suffix}"] = value if value
+        headers["X-Slimmer-#{header_suffix}"] = value.to_s if value
       end
     end
   end
--- lib/slimmer/version.rb
+++ lib/slimmer/version.rb
@@ -1,3 +1,3 @@
 module Slimmer
-  VERSION = '1.1.20'
+  VERSION = '1.1.21'
 end
