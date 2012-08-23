c45105c920b952fc9a26dcf7a22113fd3d232575
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-08-23 11:06:58 +0100

    Re-order autoload statements.

--- lib/slimmer.rb
+++ lib/slimmer.rb
@@ -18,18 +18,18 @@ module Slimmer
   autoload :Headers, 'slimmer/headers'
 
   module Processors
-    autoload :TitleInserter, 'slimmer/processors/title_inserter'
     autoload :AdminTitleInserter, 'slimmer/processors/admin_title_inserter'
-    autoload :SectionInserter, 'slimmer/processors/section_inserter'
-    autoload :TagMover, 'slimmer/processors/tag_mover'
+    autoload :BodyClassCopier, 'slimmer/processors/body_class_copier'
+    autoload :BodyInserter, 'slimmer/processors/body_inserter'
     autoload :ConditionalCommentMover, 'slimmer/processors/conditional_comment_mover'
     autoload :FooterRemover, 'slimmer/processors/footer_remover'
-    autoload :BodyInserter, 'slimmer/processors/body_inserter'
-    autoload :BodyClassCopier, 'slimmer/processors/body_class_copier'
-    autoload :HeaderContextInserter, 'slimmer/processors/header_context_inserter'
     autoload :GoogleAnalyticsConfigurator, 'slimmer/processors/google_analytics_configurator'
+    autoload :HeaderContextInserter, 'slimmer/processors/header_context_inserter'
     autoload :RelatedItemsInserter, 'slimmer/processors/related_items_inserter'
     autoload :SearchPathSetter, 'slimmer/processors/search_path_setter'
+    autoload :SectionInserter, 'slimmer/processors/section_inserter'
+    autoload :TagMover, 'slimmer/processors/tag_mover'
+    autoload :TitleInserter, 'slimmer/processors/title_inserter'
   end
 
   class TemplateNotFoundException < StandardError; end
