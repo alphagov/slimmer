633f5abf51e27aff0faa7023f82fde257768e8b8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-09-06 16:41:02 +0100

    Support any .raw template in test mode

--- lib/slimmer/test.rb
+++ lib/slimmer/test.rb
@@ -5,8 +5,8 @@ module Slimmer
   class Skin
     def load_template name
       logger.debug "Slimmer: TEST MODE - Loading fixture template from #{__FILE__}"
-      if name == 'related.raw'
-        %{<div id="test-related-items"></div>}
+      if name =~ /\A(.*)\.raw\z/
+        %{<div id="test-#{$1}"></div>}
       else
         Slimmer::TestTemplate::TEMPLATE
       end
