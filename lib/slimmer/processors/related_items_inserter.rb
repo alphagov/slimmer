1cfbe2be8267be9d1f264decc3bcde279f629218
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-09-12 13:42:44 +0000

    Use the correct section for an artefact
    When using the Panopticon API, it was the root section, so let's reinstate that.

--- lib/slimmer/processors/related_items_inserter.rb
+++ lib/slimmer/processors/related_items_inserter.rb
@@ -14,9 +14,28 @@ class Slimmer::Processors::RelatedItemsInserter
   
   def related_item_block
     artefact = @artefact
+    root_primary_section = root_primary_section(@artefact)
     is_content_api_artefact = artefact.has_key?("related")
     related_block_template = @skin.template('related.raw')
     html = ERB.new(related_block_template).result(binding)
     Nokogiri::HTML.fragment(html)
   end
+
+  # Duplicated in Frontend
+  def root_primary_section(artefact)
+    primary_section = artefact["tags"].detect do |tag| 
+      tag["details"]["type"] == "section"
+    end
+
+    if primary_section.nil?
+      nil
+    else
+      if primary_section["parent"]
+        primary_section["parent"]
+      else
+        primary_section
+      end
+    end
+  end
+
 end
