76cdd04be154b43e3ab8d30058b57f2818221847
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-09-17 13:45:04 +0000

    Refactor skin test to use Minitest:Spec

--- test/skin_test.rb
+++ test/skin_test.rb
@@ -1,61 +1,61 @@
 require_relative "test_helper"
 
-class SkinTest < MiniTest::Unit::TestCase
-  def test_template_can_be_loaded
-    skin = Slimmer::Skin.new asset_host: "http://example.local/"
-    expected_url = "http://example.local/templates/example.html.erb"
-    stub_request(:get, expected_url).to_return :body => "<foo />"
-
-    template = skin.template 'example'
-
-    assert_requested :get, "http://example.local/templates/example.html.erb"
-    assert_equal "<foo />", template
-  end
-
-  def test_templates_can_be_cached
-    skin = Slimmer::Skin.new asset_host: "http://example.local/", use_cache: true
-    expected_url = "http://example.local/templates/example.html.erb"
-    stub_request(:get, expected_url).to_return :body => "<foo />"
-
-    first_access = skin.template 'example'
-    second_access = skin.template 'example'
-
-    assert_requested :get, "http://example.local/templates/example.html.erb", times: 1
-    assert_same first_access, second_access
-  end
-
-  def test_should_raise_appropriate_exception_when_template_not_found
-    skin = Slimmer::Skin.new asset_host: "http://example.local/"
-    expected_url = "http://example.local/templates/example.html.erb"
-    stub_request(:get, expected_url).to_return(:status => '404')
-
-    assert_raises(Slimmer::TemplateNotFoundException) do
-      skin.template 'example'
-    end
-  end
-
-  def test_should_raise_appropriate_exception_when_cant_reach_template_host
-    skin = Slimmer::Skin.new asset_host: "http://example.local/"
-    expected_url = "http://example.local/templates/example.html.erb"
-    stub_request(:get, expected_url).to_raise(Errno::ECONNREFUSED)
-
-    assert_raises(Slimmer::CouldNotRetrieveTemplate) do
-      skin.template 'example'
-    end
-  end
-
-  def test_should_raise_appropriate_exception_when_hostname_cannot_be_resolved
-    skin = Slimmer::Skin.new asset_host: "http://non-existent.domain/"
-    expected_url = "http://non-existent.domain/templates/example.html.erb"
-    stub_request(:get, expected_url).to_raise(SocketError)
-
-    assert_raises(Slimmer::CouldNotRetrieveTemplate) do
-      skin.template 'example'
-    end
-  end
-end
-
 describe Slimmer::Skin do
+
+  describe "loading templates" do
+    it "should be able to load the template" do
+      skin = Slimmer::Skin.new asset_host: "http://example.local/"
+      expected_url = "http://example.local/templates/example.html.erb"
+      stub_request(:get, expected_url).to_return :body => "<foo />"
+
+      template = skin.template 'example'
+
+      assert_requested :get, "http://example.local/templates/example.html.erb"
+      assert_equal "<foo />", template
+    end
+
+    it "should cache the template" do
+      skin = Slimmer::Skin.new asset_host: "http://example.local/", use_cache: true
+      expected_url = "http://example.local/templates/example.html.erb"
+      stub_request(:get, expected_url).to_return :body => "<foo />"
+
+      first_access = skin.template 'example'
+      second_access = skin.template 'example'
+
+      assert_requested :get, "http://example.local/templates/example.html.erb", times: 1
+      assert_same first_access, second_access
+    end
+
+    it "should raise appropriate exception when template not found" do
+      skin = Slimmer::Skin.new asset_host: "http://example.local/"
+      expected_url = "http://example.local/templates/example.html.erb"
+      stub_request(:get, expected_url).to_return(:status => '404')
+
+      assert_raises(Slimmer::TemplateNotFoundException) do
+        skin.template 'example'
+      end
+    end
+
+    it "should raise appropriate exception when cant reach template host" do
+      skin = Slimmer::Skin.new asset_host: "http://example.local/"
+      expected_url = "http://example.local/templates/example.html.erb"
+      stub_request(:get, expected_url).to_raise(Errno::ECONNREFUSED)
+
+      assert_raises(Slimmer::CouldNotRetrieveTemplate) do
+        skin.template 'example'
+      end
+    end
+
+    it "should raise appropriate exception when hostname cannot be resolved" do
+      skin = Slimmer::Skin.new asset_host: "http://non-existent.domain/"
+      expected_url = "http://non-existent.domain/templates/example.html.erb"
+      stub_request(:get, expected_url).to_raise(SocketError)
+
+      assert_raises(Slimmer::CouldNotRetrieveTemplate) do
+        skin.template 'example'
+      end
+    end
+  end
 
   describe "parsing artefact from header" do
     before do
