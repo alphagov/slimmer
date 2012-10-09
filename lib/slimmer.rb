5a525c1d51b7a9c278492473b2ef50ae4bb080d0
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-10-09 18:07:53 +0000

    Remove meta viewport HTML if relevant header set
    As an app that doesn't require a mobile view, remove the <meta viewport>
    html that is in static. The main reason this commit exists is because
    trade tariff doesn't need a mobile view.
    
    However, the meta viewport html sets mobile devices so that they adjust
    all content to fit their screen size and to turn off zoom. This allows
    apps that don't want to enforce this the ability to turn it off using a
    simple slimmer header.

--- lib/slimmer.rb
+++ lib/slimmer.rb
@@ -33,6 +33,7 @@ module Slimmer
     autoload :GoogleAnalyticsConfigurator, 'slimmer/processors/google_analytics_configurator'
     autoload :HeaderContextInserter, 'slimmer/processors/header_context_inserter'
     autoload :LogoClassInserter, 'slimmer/processors/logo_class_inserter'
+    autoload :MetaViewportRemover, 'slimmer/processors/meta_viewport_remover'
     autoload :RelatedItemsInserter, 'slimmer/processors/related_items_inserter'
     autoload :ReportAProblemInserter, 'slimmer/processors/report_a_problem_inserter'
     autoload :SearchIndexSetter, 'slimmer/processors/search_index_setter'
