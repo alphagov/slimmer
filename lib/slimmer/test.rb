f3152057785e61d7130d7adb177dfbf7bfcae686
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-10-12 14:09:21 +0000

    Add a test case helper for campaigns

--- lib/slimmer/test.rb
+++ lib/slimmer/test.rb
@@ -5,7 +5,7 @@ module Slimmer
   class Skin
     def load_template name
       logger.debug "Slimmer: TEST MODE - Loading fixture template from #{__FILE__}"
-      if name =~ /\A(.*)\.raw\z/
+      if name =~ /\A(.*)\.raw\z/ || name == "campaign"
         %{<div id="test-#{$1}"></div>}
       else
         Slimmer::TestTemplate::TEMPLATE
