52d570fb9417e5c16434eafd8ed3cf507b3b7418
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-10-12 15:52:59 +0000

    Add a separate else case for campaign templates

--- lib/slimmer/test.rb
+++ lib/slimmer/test.rb
@@ -5,8 +5,10 @@ module Slimmer
   class Skin
     def load_template name
       logger.debug "Slimmer: TEST MODE - Loading fixture template from #{__FILE__}"
-      if name =~ /\A(.*)\.raw\z/ || name == "campaign"
+      if name =~ /\A(.*)\.raw\z/
         %{<div id="test-#{$1}"></div>}
+      else if name == "campaign"
+        %{<section class="black" id="campaign-notification"><div><p>You've got notifications!</p></div></section>}
       else
         Slimmer::TestTemplate::TEMPLATE
       end
