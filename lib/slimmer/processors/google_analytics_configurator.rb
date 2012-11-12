4930c998d6f2cc5d044268b6cd3c6d8b3809b4ba
Chris Parsons <chris@thinkcodelearn.com>
2012-11-12 14:41:17 +0000

    Move organisations header out of artefact block
    Discovered that Whitehall isn't sending an artefact block, and that
    therefore we're setting this header incorrectly. Best to set manually
    for now, as sending an artefact would require another layer of
    decorator indirection in order to correctly deduce the organisations.

--- lib/slimmer/processors/google_analytics_configurator.rb
+++ lib/slimmer/processors/google_analytics_configurator.rb
@@ -15,8 +15,8 @@ module Slimmer::Processors
         custom_vars << set_custom_var_downcase(1, "Section", @artefact.primary_root_section["title"]) if @artefact.primary_root_section
         custom_vars << set_custom_var_downcase(3, "NeedID", @artefact.need_id)
         custom_vars << set_custom_var_downcase(4, "Proposition", (@artefact.business_proposition ? 'business' : 'citizen')) unless @artefact.business_proposition.nil?
-        custom_vars << set_custom_var(9, "Organisations", @artefact.organisations) unless @artefact.organisations.nil?
       end
+      custom_vars << set_custom_var(9, "Organisations", @headers[Slimmer::Headers::ORGANISATIONS_HEADER])
       custom_vars << set_custom_var_downcase(2, "Format", @headers[Slimmer::Headers::FORMAT_HEADER])
       custom_vars << set_custom_var_downcase(5, "ResultCount", @headers[Slimmer::Headers::RESULT_COUNT_HEADER])
 
@@ -32,6 +32,7 @@ module Slimmer::Processors
     end
 
     def set_custom_var(slot, name, value)
+      return nil unless value
       response = "_gaq.push(#{JSON.dump([ "_setCustomVar", slot, name, value, PAGE_LEVEL_EVENT])});\n"
       response + "GOVUK.Analytics.#{name} = \"#{value}\";"
     end
