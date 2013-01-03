082e7bb81a66efa896ed0867bb789346086aa4f8
James Stewart <jys@ketlai.co.uk>
2013-01-03 11:05:45 +0000

    Rack slimmer app now does end to end skinning
    There are some hardcoded pieces like the logger and the asset host which will need to go, but
    we do now have a working end-to-end rack app that provides the majority of functionality.

--- app/slimmer_app.rb
+++ app/slimmer_app.rb
@@ -1,17 +1,49 @@
 require 'json'
+require 'slimmer/skin'
+require 'slimmer/headers'
+require 'slimmer/artefact'
+require 'logger'
+require 'erb'
+require 'nokogiri'
+
+module Slimmer
+  module Processors
+  end
+end
+
+Dir["../lib/slimmer/processors/*.rb"].each { |proc| require proc }
 
 class SlimmerApp
   POST_BODY = 'rack.input'.freeze
+  attr_accessor :logger, :skin
+
+  def initialize
+    self.logger = Logger.new(STDOUT)
+    self.skin = Slimmer::Skin.new(asset_host: 'https://static.preview.alphagov.co.uk/', logger: self.logger)
+  end
 
   def call(env)
     if env['REQUEST_METHOD'] !~ %r{POST}i
       [406, {"Content-Type" => "text/plain"}, ["Not Acceptable"]]
     elsif env['CONTENT_TYPE'] =~ %r{application/json}i
       encoded_body = env[POST_BODY].read
-      body = JSON.parse(encoded_body)
-      [200, {"Content-Type" => "text/html"}, [body.inspect]]
+      [200, {"Content-Type" => "text/html"}, [process(encoded_body)]]
     else
       [404, {"Content-Type" => "text/plain"}, ["I prefer JSON"]]
-    end    
+    end
+  end
+
+  def process(encoded_body)
+    body = JSON.parse(encoded_body)
+    html = body.delete('source_content')
+    slim(html, body)
+  end
+
+  def slim(source_content, artefact_data)
+    artefact = Slimmer::Artefact.new(artefact_data)
+    headers = {}
+    template_name = 'wrapper'
+    context_url = 'http://www.example.com/blah'
+    skin.process_success(headers, artefact, source_content, template_name, context_url)
   end
 end
--- app/test/app_test.rb
+++ app/test/app_test.rb
@@ -9,6 +9,11 @@ class AppTest < MiniTest::Unit::TestCase
     SlimmerApp.new
   end
 
+  def post_as_json(hash)
+    header "Content-Type", "application/json"
+    post "/", JSON.dump(hash)
+  end
+
   def test_returns_406_on_get
     get "/", {}, {'Content-Type' => 'application/json'}
     assert_equal 406, last_response.status
@@ -17,5 +22,13 @@ class AppTest < MiniTest::Unit::TestCase
   def test_returns_404_when_not_json
     post "/"
     assert_equal 404, last_response.status
+  end
+
+  def test_can_skin_successfully
+    source_content = '<div id="wrapper"><p>Hello World</p></div>'
+
+    post_as_json('source_content' => source_content)
+    assert_equal 200, last_response.status
+    assert_match /GOV.UK - The best place to find government services and information/, last_response.body
   end
 end
