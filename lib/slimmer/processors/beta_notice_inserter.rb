42705750edcfd6c77df6143f6c23f3b51d1365e5
Edd Sowden <e@e26.co.uk>
2013-02-27 14:42:18 +0000

    Add js-footer class to footer beta notice
    This will let us simplify the logic we are using in JavaScript to work
    out which is the last element on the page which fixed elements can
    scroll to.

--- lib/slimmer/processors/beta_notice_inserter.rb
+++ lib/slimmer/processors/beta_notice_inserter.rb
@@ -12,13 +12,19 @@ module Slimmer::Processors
           header.add_next_sibling(beta_notice_block)
         end
         if footer = page_template.at_css('footer#footer')
-          footer.add_previous_sibling(beta_notice_block)
+          footer.add_previous_sibling(add_footer_class(beta_notice_block))
         end
       end
     end
 
     def should_add_beta_notice?
       !! @headers[Slimmer::Headers::BETA_HEADER]
+    end
+
+    def add_footer_class(block)
+      block = Nokogiri::HTML.fragment(block)
+      block.child['class'] = "#{block.child['class']} js-footer"
+      block
     end
 
     def beta_notice_block
