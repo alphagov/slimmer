c6acb1d08c7f2503c68ac3b996da8d2913386c7d
Will Tomlins <will.tomlins@unboxedconsulting.com>
2013-10-08 10:54:52 +0100

    Updated TestTemplateDependencyOnStaticTest

--- test/test_template_dependency_on_static_test.rb
+++ test/test_template_dependency_on_static_test.rb
@@ -1,22 +1,25 @@
 require "test_helper"
-require "slimmer/test_template"
 
 class TestTemplateDependencyOnStaticTest < MiniTest::Unit::TestCase
-  def test_test_template_static_asset_urls_should_be_valid
-    doc = Nokogiri::HTML.fragment(Slimmer::TestTemplate::TEMPLATE)
-    scripts = doc.search("script").map { |node| node.attributes["src"].value }
-    failing_scripts = allowing_real_web_connections do
-      scripts.select do |script_src|
-        uri = URI.parse(script_src)
-        http = Net::HTTP.new(uri.host, uri.port)
-        if uri.scheme == "https"
-          http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  def test_scripts_on_static_referenced_in_test_templates_exist
+    template_path = File.dirname(__FILE__).gsub('/test', '/lib/slimmer/test_templates')
+    Dir.foreach(template_path) do | template_file_name |
+      next if ['.','..'].include? template_file_name
+      doc = Nokogiri::HTML.fragment(File.read(File.join(template_path, template_file_name)))
+      scripts = doc.search("script").map { |node| node.attributes["src"].value }
+      failing_scripts = allowing_real_web_connections do
+        scripts.select do |script_src|
+          uri = URI.parse(script_src)
+          http = Net::HTTP.new(uri.host, uri.port)
+          if uri.scheme == "https"
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          end
+          response = http.get(uri.request_uri)
+          response.code != "200"
         end
-        response = http.get(uri.request_uri)
-        response.code != "200"
       end
+      assert failing_scripts.empty?, "Some scripts could not be loaded: #{failing_scripts.inspect}"
     end
-    assert failing_scripts.empty?, "Some scripts could not be loaded: #{failing_scripts.inspect}"
   end
 end
