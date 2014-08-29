559cb5399daa69d332cc996c4d073898d9943e72
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-08-29 11:11:58 +0000

    Don't swallow exceptions when running in test
    Previously, exceptions from processors were always swallowed, and there
    are several tests which trigger such exceptions but don't report them.
    We want these exceptions to be reported in test and dev environments so
    that we can fix them; continuing to swallow the exceptions in production
    is probably a good thing.
    
    Exceptions in production/preview are reported to errbit, as before.

--- lib/slimmer/skin.rb
+++ lib/slimmer/skin.rb
@@ -93,6 +93,7 @@ module Slimmer
           if defined?(Airbrake)
             Airbrake.notify_or_ignore(e, rack_env: rack_env)
           end
+          raise if strict
         end
         processor_end_time = Time.now
         process_time = processor_end_time - processor_start_time
