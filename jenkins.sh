be4c0179b2b207aabce22d3ab6273d190178c213
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2015-02-12 16:56:52 +0000

    Clean the repo before each build
    Without this command, files from previous builds stick around, such as
    the built gems.
    
    Because we're now cleaning the workspace, we no longer need to remove
    `Gemfile.lock` explicitly.

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 set -e
 
+git clean -fdx
+
 # Try to merge master into the current branch, and abort if it doesn't exit
 # cleanly (ie there are conflicts). This will be a noop if the current branch
 # is master.
 git merge --no-commit origin/master || git merge --abort
 
-rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake test --trace
 
