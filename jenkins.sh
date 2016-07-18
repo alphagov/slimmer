5ad1abc0ca0aa152e5029b76c3b6a4d9889463ba
Andrew Garner <andrew.garner@digital.cabinet-office.gov.uk>
2016-07-18 11:28:44 +0100

    Configure Jenkins build matrix
    This introduces a matrix of builds under CI allowing us to test our gem
    across a range of Ruby and Rails versions. This gives us confidence
    that our gem will support a range of runtimes and host applications.
    
    Our dependencies are not compatible across the full matrix so we need
    to constrain them using different Gemfiles.

--- jenkins.sh
+++ jenkins.sh
@@ -8,15 +8,38 @@ git clean -fdx
 # is master.
 git merge --no-commit origin/master || git merge --abort
 
-for version in 2.2 2.1 1.9.3; do
-  export RBENV_VERSION=$version
-  echo "Running tests under ruby $version"
+function test() {
+  local rails_version="$1"
+  local ruby_version="$2"
+
+  export BUNDLE_GEMFILE="./gemfiles/Gemfile.rails-${rails_version}"
+  export RBENV_VERSION="${ruby_version}"
+
   bundle install --path "${HOME}/bundles/${JOB_NAME}"
   bundle exec rake test --trace
-done
 
-unset RBENV_VERSION
+  unset RBENV_VERSION
+}
 
+test 5.0.x 2.3
+test 5.0.x 2.2
+
+test 4.2.x 2.3
+test 4.2.x 2.2
+test 4.2.x 2.1
+test 4.2.x 1.9.3
+
+test 4.1.x 2.3
+test 4.1.x 2.2
+test 4.1.x 2.1
+test 4.1.x 1.9.3
+
+test 3.2.x 2.3
+test 3.2.x 2.2
+test 3.2.x 2.1
+test 3.2.x 1.9.3
+
+bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec govuk-lint-ruby --diff --cached --format clang bin lib test
 
 if [[ -n "$PUBLISH_GEM" ]]; then
