3198757943ab890d40774db2d859396f5b956343
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2016-07-21 16:16:38 +0100

    Chain slimmer I18nBackend to frontend apps

--- lib/slimmer/railtie.rb
+++ lib/slimmer/railtie.rb
@@ -9,6 +9,8 @@ module Slimmer
       slimmer_config = slimmer_config.reverse_merge(app_name: app_name)
 
       app.middleware.use Slimmer::App, slimmer_config
+
+      I18n.backend = I18n::Backend::Chain.new(I18n.backend, Slimmer::I18nBackend.new)
     end
   end
 end
