555b16f82cc45f73fecc2a750376a6c2248ba056
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2016-07-26 14:32:14 +0100

    Move init of template i18n to SharedTemplates to reduce unnecessary calls

--- lib/slimmer/shared_templates.rb
+++ lib/slimmer/shared_templates.rb
@@ -5,6 +5,8 @@ module Slimmer
     end
 
     def add_shared_templates
+      I18n.backend = I18n::Backend::Chain.new(I18n.backend, Slimmer::I18nBackend.new)
+
       append_view_path Slimmer::ComponentResolver.new
     end
   end
