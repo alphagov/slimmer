2e7c575d646fb5a9343e056e1f513022444e5b65
Andrew Garner <andrew@andrewgarner.com>
2016-08-10 08:02:25 +0100

    Stub requests to template locales regardless of headers
    The headers sent are not important in this context. We want to stub
    all requests to these resources regardless of the headers sent.

--- lib/slimmer/test_helpers/shared_templates.rb
+++ lib/slimmer/test_helpers/shared_templates.rb
@@ -3,10 +3,8 @@ module Slimmer
     module SharedTemplates
       def stub_shared_component_locales
         stub_request(:get, /https?:\/\/\S+.gov.uk\/templates\/locales\/.+/).
-          with(headers: { 'Accept' => '*/*; q=0.5, application/xml', 'Accept-Encoding' => 'gzip, deflate', 'User-Agent' => 'Ruby' }).
           to_return(status: 400, headers: {})
         stub_request(:get, /https?:\/\/\S+.gov.uk\/templates\/locales\/en/).
-          with(headers: { 'Accept' => '*/*; q=0.5, application/xml', 'Accept-Encoding' => ' gzip, deflate', 'User-Agent' => 'Ruby' }).
           to_return(status: 200, body: '{}', headers: {})
       end
 
