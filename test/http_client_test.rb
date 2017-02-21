272662413916bb79ef3b1e0bb6daacbeab55a3a2
Tijmen Brommet <tijmen@gmail.com>
2017-02-21 23:18:17 +0000

    Add a user agent header for all requests
    This will make it easier to see who is making the requests.
    
    The format for the UA is consistent with gds-api-adapters.

---
+++ test/http_client_test.rb
@@ -0,0 +1,17 @@
+require_relative "test_helper"
+
+describe Slimmer::HTTPClient do
+  describe ".get" do
+    it "adds the correct user agent header" do
+      ENV['GOVUK_APP_NAME'] = 'my-app'
+
+      request = stub_request(:get, "https://example.com/").
+        with(headers: { 'User-Agent' => 'slimmer/10.0.0 (my-app)' }).
+        to_return(status: 200)
+
+      Slimmer::HTTPClient.get("https://example.com")
+
+      assert_requested(request)
+    end
+  end
+end
