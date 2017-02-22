989ca787e2267c1871bc1f703bb82347b06630d4
Tijmen Brommet <tijmen@gmail.com>
2017-02-22 14:38:23 +0000

    Make sure that our tests pass for all versions

--- test/http_client_test.rb
+++ test/http_client_test.rb
@@ -6,7 +6,7 @@ describe Slimmer::HTTPClient do
       ENV['GOVUK_APP_NAME'] = 'my-app'
 
       request = stub_request(:get, "https://example.com/").
-        with(headers: { 'User-Agent' => 'slimmer/10.0.0 (my-app)' }).
+        with(headers: { 'User-Agent' => "slimmer/#{Slimmer::VERSION} (my-app)" }).
         to_return(status: 200)
 
       Slimmer::HTTPClient.get("https://example.com")
