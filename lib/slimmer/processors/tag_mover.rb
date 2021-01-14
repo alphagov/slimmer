9f818656c0a748c5496e9e66286ff5ba809c2fae
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2021-01-14 10:11:48 +0000

    Ensure async and defer scripts are hoisted to the top of the head
    This prevents `defer` or `async` script tags being automatically
    moved to the bottom of the `body` as with other script tags.
    
    Instead, these scripts will be hoisted to the top of the `head`,
    above blocking tags such as CSS.
    
    This will allow them to be fetched as soon as possible without
    blocking parsing.

--- lib/slimmer/processors/tag_mover.rb
+++ lib/slimmer/processors/tag_mover.rb
@@ -1,11 +1,11 @@
 module Slimmer::Processors
   class TagMover
     def filter(src, dest)
-      move_tags(src, dest, "script", dest_node: "body", keys: %w[src inner_html])
       move_tags(src, dest, "link",   must_have: %w[href])
       move_tags(src, dest, "meta",   must_have: %w[name content], keys: %w[name content http-equiv], insertion_location: :top)
       move_tags(src, dest, "meta",   must_have: %w[property content], keys: %w[property content], insertion_location: :top)
       move_tags(src, dest, "base",   must_have: %w[href])
+      move_tags(src, dest, "script", keys: %w[src inner_html], head_if_attributes: %w[async defer])
     end
 
     def include_tag?(node, min_attrs)
@@ -31,21 +31,34 @@ module Slimmer::Processors
       node
     end
 
+    def head_or_body(node, head_if_attributes)
+      if head_if_attributes.any? { |attribute| node.has_attribute?(attribute) }
+        "head"
+      else
+        "body"
+      end
+    end
+
     def move_tags(src, dest, type, opts)
       comparison_attrs = opts[:keys] || opts[:must_have]
       min_attrs = opts[:must_have] || []
+      head_if_attributes = opts[:head_if_attributes] || []
+      dest_node = "head"
       already_there = dest.css(type).map { |node|
         tag_fingerprint(node, comparison_attrs)
       }.compact
-      dest_node = opts[:dest_node] || "head"
 
       src.css(type).each do |node|
         next unless include_tag?(node, min_attrs) && !already_there.include?(tag_fingerprint(node, comparison_attrs))
 
         node = wrap_node(src, node)
+        if head_if_attributes.any?
+          dest_node = head_or_body(node, head_if_attributes)
+          insert_at_top = true if dest_node == "head"
+        end
         node.remove
 
-        if opts[:insertion_location] == :top
+        if opts[:insertion_location] == :top || insert_at_top
           dest.at_xpath("/html/#{dest_node}").prepend_child(node)
         else
           dest.at_xpath("/html/#{dest_node}") << node
