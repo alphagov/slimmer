eca81b7f49f3379b114d74db9863ca66f89c1159
Ian James <ian.james@digital.cabinet-office.gov.uk>
2021-07-01 12:13:45 +0100

    Fixes and changes
     - use guard clause
     - use rack for mocking request object
     - add test for email redaction
     - add test for string encoding
     - merge encoding and redaction methods

--- lib/slimmer/processors/feedback_url_swapper.rb
+++ lib/slimmer/processors/feedback_url_swapper.rb
@@ -1,28 +1,30 @@
 module Slimmer::Processors
   class FeedbackURLSwapper
-    def initialize(request, headers = {})
+    def initialize(request, headers)
       @headers = headers
       @request = request
     end
 
     def filter(_src, dest)
-      email_regex = /[^\s=\/?&]+(?:@|%40)[^\s=\/?&]+/
+      return dest unless is_gem_layout?
 
-      original_url = @request.base_url + @request.fullpath
-      original_url_without_pii = utf_encode(original_url.gsub(email_regex, "[email]"))
-      dest.at_css(".gem-c-feedback input[name='url']").set_attribute("value", original_url_without_pii) if is_gem_layout?
+      original_url_without_pii = remove_pii(@request.base_url + @request.fullpath)
+      dest.at_css(".gem-c-feedback input[name='url']").set_attribute("value", original_url_without_pii)
 
-      full_path = @request.fullpath
-      full_path_without_pii = utf_encode(full_path.gsub(email_regex, "[email]"))
-      dest.at_css(".gem-c-feedback input[name='email_survey_signup[survey_source]']").set_attribute("value", full_path_without_pii) if is_gem_layout?
+      full_path_without_pii = remove_pii(@request.fullpath)
+      dest.at_css(".gem-c-feedback input[name='email_survey_signup[survey_source]']").set_attribute("value", full_path_without_pii)
 
       dest
     end
 
   private
 
-    def utf_encode(element)
-      element.is_a?(String) ? element.encode : element
+    # This PII removal is also found in the [feedback component in the GOV.UK
+    # Publishing Components gem](https://git.io/JcCIE), and any changes made
+    # need to be kept in sync.
+    def remove_pii(string)
+      email_regex = /[^\s=\/?&]+(?:@|%40)[^\s=\/?&]+/
+      string.force_encoding("UTF-8").gsub(email_regex, "[email]")
     end
 
     def is_gem_layout?
--- test/processors/feedback_url_swapper_test.rb
+++ test/processors/feedback_url_swapper_test.rb
@@ -23,19 +23,10 @@ class FeedbackURLSwapperTest < MiniTest::Test
       </html>
     )
 
-    request = {}
+    env = Rack::MockRequest.env_for("https://new-example.com/new_path")
+    request = Rack::Request.new(env)
 
-    def request.base_url
-      "https://new-example.com"
-    end
-
-    def request.fullpath
-      "/new_path"
-    end
-
-    headers = {
-      Slimmer::Headers::TEMPLATE_HEADER => "gem_layout",
-    }
+    headers = { Slimmer::Headers::TEMPLATE_HEADER => "gem_layout" }
 
     Slimmer::Processors::FeedbackURLSwapper.new(request, headers).filter(nil, template)
 
@@ -65,23 +56,77 @@ class FeedbackURLSwapperTest < MiniTest::Test
       </html>
     )
 
-    request = {}
+    env = Rack::MockRequest.env_for("https://new-example.com/new_path")
+    request = Rack::Request.new(env)
 
-    def request.base_url
-      "https://new-example.com"
-    end
-
-    def request.fullpath
-      "/new_path"
-    end
-
-    headers = {
-      Slimmer::Headers::TEMPLATE_HEADER => "core_layout",
-    }
+    headers = { Slimmer::Headers::TEMPLATE_HEADER => "core_layout" }
 
     Slimmer::Processors::FeedbackURLSwapper.new(request, headers).filter(nil, template)
 
     assert_in template, "#test-input[value='/old_path']"
     assert_in template, "#test-input-two[value='https://example.com/old_path']"
+  end
+
+  def test_should_redact_an_email_adress_in_the_url
+    template = as_nokogiri %(
+      <html>
+        <body>
+          <div class="gem-c-feedback">
+            <input
+              id="test-input"
+              type="hidden"
+              name="email_survey_signup[survey_source]"
+              value="/old_path"
+            >
+            <input
+              id="test-input-two"
+              type="hidden"
+              name="url"
+              value="https://example.com/old_path"
+            >
+          </div>
+        </body>
+      </html>
+    )
+
+    env = Rack::MockRequest.env_for("https://new-example.com/new_path?secret@email.com")
+    request = Rack::Request.new(env)
+
+    headers = { Slimmer::Headers::TEMPLATE_HEADER => "gem_layout" }
+
+    Slimmer::Processors::FeedbackURLSwapper.new(request, headers).filter(nil, template)
+
+    assert_in template, "#test-input[value='/new_path?[email]']"
+    assert_in template, "#test-input-two[value='https://new-example.com/new_path?[email]']"
+  end
+
+  def test_should_cope_with_a_ascii_encoded_url
+    template = as_nokogiri %(
+      <div class="gem-c-feedback">
+        <input
+          id="test-input"
+          name="email_survey_signup[survey_source]"
+          value="/old_path"
+        >
+        <input
+          id="test-input-two"
+          name="url"
+          value="https://example.com/old_path"
+        >
+      </div>
+    )
+
+    # Need to use a stub as a MockRequest will throw an exception on ASCII
+    # characters.
+    request = stub("Rack::Request",
+                   base_url: "https://example.com".force_encoding("ASCII-8BIT"),
+                   fullpath: "/test?áscii=%EE%90%80".force_encoding("ASCII-8BIT"))
+
+    headers = { Slimmer::Headers::TEMPLATE_HEADER => "gem_layout" }
+
+    Slimmer::Processors::FeedbackURLSwapper.new(request, headers).filter(nil, template)
+
+    assert_in template, "#test-input[value='/test?áscii=%EE%90%80']"
+    assert_in template, "#test-input-two[value='https://example.com/test?áscii=%EE%90%80']"
   end
 end
